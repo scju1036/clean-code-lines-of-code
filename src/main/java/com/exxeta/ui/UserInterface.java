1dfb498d8860c5208c1a2a0d5a8a1afb7b9d299d
Miguel Costa <miguel.costa@exxeta.com>
2020-01-23 15:08:39 +0100

    Added UI

---
+++ src/main/java/com/exxeta/ui/UserInterface.java
@@ -0,0 +1,22 @@
+package com.exxeta.ui;
+
+import com.exxeta.domain.LOCStat;
+
+import java.util.List;
+
+public class UserInterface {
+
+    public static void  printLocCount(List<LOCStat> locStats){
+        long total = 0;
+        long linesOfCode = 0;
+
+        for (LOCStat locStat : locStats) {
+            System.out.println(locStat.getFileName() + " " + locStat.getTotalLines() + locStat.getLinesOfCode());
+            total += locStat.getTotalLines();
+            linesOfCode += locStat.getLinesOfCode();
+        }
+
+        System.out.println("Total: " + total);
+        System.out.println("Total Lines of Code: " + linesOfCode);
+    }
+}
