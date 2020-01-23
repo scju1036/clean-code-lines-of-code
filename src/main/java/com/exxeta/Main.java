f11bf8a8fcd8e3d94c286af0420021c385e1c2c5
Julian Schnurr <julian.schnurr@exxeta.de>
2020-01-23 13:49:14 +0100

    add initial structure

---
+++ src/main/java/com/exxeta/Main.java
@@ -0,0 +1,9 @@
+package com.exxeta;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("N(ext page, P(revious page, F(irst page, L(ast page, eX(it");
+    }
+}
